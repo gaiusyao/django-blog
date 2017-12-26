ef470d24f4ec1ff07f4b04ac9c456e488573a0d0
gaiusyao <gaiusyao@yeah.net>
2017-12-26 16:39:18 +0800

    add detail, markdown, highlight

--- blogproject/gaius_blog/models.py
+++ blogproject/gaius_blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # 分类
 class Category(models.Model):
@@ -42,3 +43,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # 自定义 get_absolute_url 方法
+    def get_absolute_url(self):
+        return reverse('gaius_blog:detail', kwargs={'pk': self.pk})
