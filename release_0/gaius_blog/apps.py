aeeec7c9adf57f639bf567b3bff02dc35f92084e
gaiusyao <gaiusyao@yeah.net>
2018-01-04 13:57:35 +0800

    Change the directory structure

--- release_0/gaius_blog/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GaiusBlogConfig(AppConfig):
-    name = 'gaius_blog'
--- release_0/gaius_blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'gaius_blog'
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
-]
